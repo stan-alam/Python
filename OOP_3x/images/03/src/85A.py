2309f37bac6624af741393921db701b24a3a0da2
Stanislaw <stan.alam@gmail.com>
2019-07-23 19:50:58 -0400

    added codeblock 85.A

---
+++ OOP_3x/images/03/src/85A.py
@@ -0,0 +1,27 @@
+# Code block 85.A
+
+class AudioFile:
+  def __init__(self, filename):
+    if not filename.endswith(self.ext):
+      raise Exception("invalid file format")
+
+      self.filename = filename
+
+class Mp3File(AudioFile):
+  extension ="mp3"
+
+  def play(self):
+    print("play { } as mp3".format(self.filename))
+
+class WavFile(AudioFile):
+  extension = "wav"
+
+  def play(self):
+    print("play { } as wav".format(self.filename))
+
+class OggFile(AudioFile):
+  extension = "oog"
+
+  def play(self):
+    print("play { } as ogg".format(self.filename))
+        
