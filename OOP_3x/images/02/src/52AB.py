4c656b7483df0c71c43affdc0d611aab595ae5e2
Stanislaw Alam <stan.alam@gmail.com>
2019-08-23 21:56:18 +0000

    added 38.A, 41A, 52A, 62A, -- still need to add 63A codeblock, and update README

---
+++ OOP_3x/images/02/src/52AB.py
@@ -0,0 +1,13 @@
+class UselessClass:
+    """This class might be useful to some modules while useless to others"""
+    
+    pass
+  
+def main():
+    """creates a useless class and does something useless unless there's something useful to do with the module?"""
+    
+    useless = UselessClass()
+    print(useless)
+   
+if __name__ == "__main__":
+   main()
