8a2743ef6888e853652e8f5a1554b502ae7a62d1
Stanislaw <stan.alam@gmail.com>
2020-03-21 08:54:17 -0400

    added cb 260A

---
+++ OOP_3x/images/08/src/260A.py
@@ -0,0 +1,20 @@
+from threading import Timer
+import datetime
+from urllib.request import urlopen
+
+class UpdatedUrl:
+    def __init__(self, url):
+        self.url = url
+        self.contents = ''
+        self.last_updated = None
+        self.update()
+
+    def update(self):
+        self.contents = urlopen(self.url).read()
+        self.last_updated = datetime.datetime.now()
+        self.schedule()
+
+    def schedule(self):
+        self.timer = Timer(3600, self.update)
+        self.timer.setDaemon(True)
+        self.timer.start()
