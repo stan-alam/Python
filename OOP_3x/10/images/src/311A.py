274b1da02ce868f42c5f04c70fdafe2f8eaac724
Stanislaw <stan.alam@gmail.com>
2020-11-02 17:36:56 -0500

    added cb 311.A

---
+++ OOP_3x/10/images/src/311A.py
@@ -0,0 +1,7 @@
+#311.A 
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("localhost", 3000))
+print("Received: {0}".format(client.recv(3000)))
+client.close()
