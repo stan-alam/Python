a89bb7322b925bc393d3b8f9e23119f55c6eb006
stanislaw alam <stan.alam@gmail.com>
2024-04-05 20:05:56 -0400

    added to fix flash cards - > django, the D is silent

---
+++ FixFlashCards/flashcards/urls.py
@@ -0,0 +1,9 @@
+# flashcards/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+ 
+urlpatterns = [
+     path("admin/", admin.site.urls),
+     path("", include("cards.urls")),
+ ]
