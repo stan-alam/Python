5b99231eae21a75fa0cd0a40cf90576ad6bd29e0
stanislaw alam <stan.alam@gmail.com>
2024-06-30 18:28:52 -0400

    added run_test.py

--- turtle_stuff/particle_sim.py
+++ turtle_stuff/particle_sim.py
@@ -2,13 +2,15 @@ import turtle
 
 class SimulationArea:
     def __init__(self, width, height, color="white"):
-        self.width = width
-        self.height = height
-        self.left_edge = -width // 2
-        self.right_edge = width // 2
-        self.top_edge = height // 2
-        self.bottom_edge = -height // 2
+        # Create a window
         self.window = turtle.Screen()
-        self.window.setup(width, height)
-        self.window.tracer(0)
-        self.window.bgcolor(color)
+
+    def update(self):
+        self.window.update()
+
+    def set_title(self, title):
+        self.window.title(title)
+        
+    @staticmethod
+    def run_mainloop():
+        turtle.mainloop()
