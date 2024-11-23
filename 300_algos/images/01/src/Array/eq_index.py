0bf184b6306423794a7c266262ead6df0aa2071f
stanislaw alam <stan.alam@gmail.com>
2024-11-23 09:19:11 -0500

    added 01 -array - eql array

--- 300_algos/images/01/src/Array/eq_index.py
+++ 300_algos/images/01/src/Array/eq_index.py
@@ -8,4 +8,8 @@ def find_equilibrium_index(arry):
         if left_sum == right_sum:
             return i
         left_sum += num
-    return - 1
+    return - 1  #no equilibrium index found
+
+arry = [-7, 1, 5, 2, -4, 3, 0]
+index = find_equilibrium_index(arry)
+print("Equilibrium index is:", {index})
