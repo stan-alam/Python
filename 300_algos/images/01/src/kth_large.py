4af1934534389e305e64b7c4e54fcdbbab664183
stanislaw alam <stan.alam@gmail.com>
2025-03-09 21:02:55 -0400

    added to Python Algo kth_largest in array

--- 300_algos/images/01/src/kth_large.py
+++ 300_algos/images/01/src/kth_large.py
@@ -13,3 +13,8 @@ print(kth_largest(arr2, k2))
 arr3 = [3, 2, 1, 5, 6, 4]
 k3 = 2
 print(kth_largest(arr3, k3))
+
+arr4 = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+k4 = 1
+print(kth_largest(arr4, k4))
+print("3rd largest in arr4 is: ", kth_largest(arr4, 3)) #what's the time complexity of this?
