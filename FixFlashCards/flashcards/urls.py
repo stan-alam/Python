afd1fa3d861a470d5f4fb3c4071ef82929abb5aa
stan alam <stan.alam@gmail.com>
2026-02-13 14:21:07 -0500

    removed flashcards

--- FixFlashCards/flashcards/urls.py
+++
@@ -1,9 +0,0 @@
-# flashcards/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
- 
-urlpatterns = [
-     path("admin/", admin.site.urls),
-     path("", include("cards.urls")),
- ]
